281d5d299894ea16c67e6b3879cb136a954952c2
Shishir Patel <shishpt@users.noreply.github.com>
2024-02-12 10:39:46 +0000

    Update navbar.vue

--- components/navbar.vue
+++ components/navbar.vue
@@ -46,7 +46,7 @@
           <div>
             <button type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
               <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full" src="Public/user.png">
+              <img class="h-8 w-8 rounded-full" src="/Public/user.png">
             </button>
           </div>
         </div>
